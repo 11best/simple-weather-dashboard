3e29be905cacec850549901d918510bfdd48d480
11best <asamaporn.b@appman.co.th>
2023-09-29 17:08:39 +0700

    create sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import Sidebar from "./components/Sidebar/Sidebar.vue";
 </script>
 
 <template>
   <Header />
+  <Sidebar />
 
-  <main>
-    <TheWelcome />
+  <main class="min-h-screen bg-pink-500 ml-60">
+    <div class="w-full bg-orange-300">ddd</div>
   </main>
 </template>
---
+++ src/components/Sidebar/Collapse.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CollapseItem from "./CollapseItem.vue";
+
+defineProps<{
+  menuInfo: { mainMenuTitle: string; menuItem: { title: string }[] };
+}>();
+</script>
+
+<template>
+  <div
+    id="sidebar-main-menu"
+    class="rounded-full cursor-pointer hover:bg-slate-300 px-4 py-2 flex justify-between"
+  >
+    <p>{{ menuInfo.mainMenuTitle }}</p>
+    <p>V</p>
+  </div>
+  <ul>
+    <CollapseItem v-for="menuItem in menuInfo.menuItem" :menu-item="menuItem" />
+  </ul>
+</template>
