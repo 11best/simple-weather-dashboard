dae37655f08717eb86ae603be4781a40fdf23883
11best <asama_b@outlook.com>
2023-09-30 16:31:54 +0700

    create TextContainer component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import Sidebar from "./components/Sidebar/Sidebar.vue";
 import ImageContainer from "./components/ImageContainer.vue";
 import GraphContainer from "./components/GraphContainer.vue";
 import IndexLevel from "./components/IndexLevel.vue";
+import TextContainer from "./components/TextContainer.vue";
+import AqiSummary from "./components/AqiSummary.vue";
 </script>
 
 <template>
@@ -13,12 +15,16 @@ import IndexLevel from "./components/IndexLevel.vue";
       <Sidebar />
       <main class="bg-pink-500 ml-64 flex-1">
         <div class="grid grid-cols-5 gap-x-4 p-4 h-full">
-          <div class="w-full col-span-3 flex flex-col gap-4">
+          <div id="left-section" class="w-full col-span-3 flex flex-col gap-4">
             <div class="h-3/6"><ImageContainer /></div>
-            <div class="h-2/6"><GraphContainer /></div>
+            <div class="h-3/6"><GraphContainer /></div>
             <div class="flex items-center"><IndexLevel /></div>
           </div>
-          <div class="w-full bg-green-300 col-span-2">ccc</div>
+          <div id="right-section" class="w-full col-span-2 p-4">
+            <AqiSummary />
+            <div><TextContainer title="Real Time Weather Conditions" /></div>
+            <div><TextContainer title="Real Air Pollution" /></div>
+          </div>
         </div>
       </main>
     </div>
---
+++ src/components/TextContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="bg-white opacity-70 w-full rounded-full text-center font-bold text-2xl py-1"
+  >
+    {{ title }}
+  </div>
+</template>
