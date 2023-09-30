392759e246f0c440f9c6be6c71abd62473e0a992
11best <asamaporn.b@appman.co.th>
2023-09-30 11:59:06 +0700

    adjust content layout and create simple index level component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar/Sidebar.vue";
 import ImageContainer from "./components/ImageContainer.vue";
 import GraphContainer from "./components/GraphContainer.vue";
+import IndexLevel from "./components/IndexLevel.vue";
 </script>
 
 <template>
@@ -12,10 +13,10 @@ import GraphContainer from "./components/GraphContainer.vue";
       <Sidebar />
       <main class="bg-pink-500 ml-72 flex-1">
         <div class="grid grid-cols-5 gap-x-4 p-4 h-full">
-          <div class="w-full col-span-3 grid grid-row-6 gap-y-4">
-            <div class="row-span-3"><ImageContainer /></div>
-            <div class="row-span-2"><GraphContainer /></div>
-            <div class="bg-teal-300 row-span-1">index</div>
+          <div class="w-full col-span-3 flex flex-col gap-4">
+            <div class="h-3/6"><ImageContainer /></div>
+            <div class="h-2/6"><GraphContainer /></div>
+            <div class="flex items-center"><IndexLevel /></div>
           </div>
           <div class="w-full bg-green-300 col-span-2">ccc</div>
         </div>
---
+++ src/components/IndexLevel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white opacity-70 w-full h-20 rounded-lg flex">
+    <div class="bg-green-300 p-2 w-24 text-sm rounded-lg">
+      title title title title
+    </div>
+    <div class="w-full flex justify-evenly">
+      <div>level 1</div>
+      <div>level 2</div>
+      <div>level 3</div>
+      <div>level 4</div>
+      <div>level 5</div>
+      <div>level 6</div>
+    </div>
+  </div>
+</template>
