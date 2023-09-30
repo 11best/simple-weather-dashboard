b322c289c5cadcf7c97625cbe91d04e63d73436f
11best <asamaporn.b@appman.co.th>
2023-09-30 18:06:41 +0700

    create level describe section

--- src/components/PollutionSection.vue
+++ src/components/PollutionSection.vue
@@ -46,11 +46,28 @@ const mockWeatherConditions = [
     detail: "papuan cute",
   },
 ];
+
+const mockLevel = [
+  { name: "normal", color: "green" },
+  { name: "crisis", color: "orange" },
+  { name: "hazard", color: "red" },
+];
 </script>
 
 <template>
   <TextContainer title="Real Air Pollution" />
   <div class="grid grid-cols-3 gap-2 mt-2">
     <CardWithLabel v-for="item in mockWeatherConditions" :data="item" />
+  </div>
+  <div
+    class="w-full bg-white opacity-70 rounded-full flex justify-evenly p-2 mt-4"
+  >
+    <div v-for="level in mockLevel" class="flex items-center">
+      <div
+        class="w-6 h-3 rounded-full"
+        :style="{ backgroundColor: level.color }"
+      ></div>
+      <p class="text-xs ml-2">{{ level.name }}</p>
+    </div>
   </div>
 </template>
