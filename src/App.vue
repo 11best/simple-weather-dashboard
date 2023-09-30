a90c6f6c79bf4aa813ec23cc369bacdfec02e271
11best <asamaporn.b@appman.co.th>
2023-09-30 11:21:57 +0700

    split content to two columns

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,10 @@ import Sidebar from "./components/Sidebar/Sidebar.vue";
     <div class="relative flex flex-grow">
       <Sidebar />
       <main class="bg-pink-500 ml-72 flex-1">
-        <div class="w-full bg-orange-300">ddd</div>
+        <div class="grid grid-cols-5 p-4 h-full">
+          <div class="w-full bg-orange-300 col-span-3">ddd</div>
+          <div class="w-full bg-green-300 col-span-2">ccc</div>
+        </div>
       </main>
     </div>
   </div>
