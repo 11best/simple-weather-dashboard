9554216a628d41de32fffcb83088ca7869f1e6aa
11best <asamaporn.b@appman.co.th>
2023-09-30 11:33:03 +0700

    create image and graph container

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar/Sidebar.vue";
+import ImageContainer from "./components/ImageContainer.vue";
+import GraphContainer from "./components/GraphContainer.vue";
 </script>
 
 <template>
@@ -9,8 +11,12 @@ import Sidebar from "./components/Sidebar/Sidebar.vue";
     <div class="relative flex flex-grow">
       <Sidebar />
       <main class="bg-pink-500 ml-72 flex-1">
-        <div class="grid grid-cols-5 p-4 h-full">
-          <div class="w-full bg-orange-300 col-span-3">ddd</div>
+        <div class="grid grid-cols-5 gap-x-4 p-4 h-full">
+          <div class="w-full col-span-3 grid grid-row-6 gap-y-4">
+            <div class="row-span-3"><ImageContainer /></div>
+            <div class="row-span-2"><GraphContainer /></div>
+            <div class="bg-teal-300 row-span-1">index</div>
+          </div>
           <div class="w-full bg-green-300 col-span-2">ccc</div>
         </div>
       </main>
---
+++ src/components/GraphContainer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-white opacity-50 w-full h-full rounded-lg">graph</div>
+</template>
